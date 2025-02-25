9a10a6aff2d07b40ae3abdfcb9db652933c7646b
Artem <artemmohov05@mail.ru>
2025-02-25 18:03:59 +0500

    Разработана страница Authorization

--- YP02/MainWindow.xaml.cs
+++ YP02/MainWindow.xaml.cs
@@ -16,9 +16,43 @@ namespace YP02
     /// </summary>
     public partial class MainWindow : Window
     {
+        public static MainWindow init;
+
         public MainWindow()
         {
             InitializeComponent();
+            init = this;
+            OpenPages(pages.authorization);
+        }
+
+        public enum pages
+        {
+            authorization,
+            main,
+            student
+        }
+
+        public void OpenPages(pages _pages)
+        {
+            this.MinHeight = 800;
+            this.MinWidth = 950;
+            this.Height = 850;
+            this.Width = 1000;
+
+            switch (_pages)
+            {
+                case pages.authorization:
+                    frame.Navigate(new Pages.Authorization());
+                    break;
+
+                case pages.main:
+                   frame.Navigate(new Pages.Main());
+                    break;
+
+                //case pages.student:
+                //    frame.Navigate(new Pages.Student());
+                //    break;
+            }
         }
     }
 }
---
+++ YP02/Pages/Authorization.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace YP02.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для Authorization.xaml
+    /// </summary>
+    public partial class Authorization : Page
+    {
+        public Authorization()
+        {
+            InitializeComponent();
+        }
+
+        private void Click_Authorization(object sender, RoutedEventArgs e)
+        {
+            //string login = UserNameTextBox.Text;
+            //string password = PasswordBox.Password;
+
+            //if (string.IsNullOrWhiteSpace(login) || string.IsNullOrWhiteSpace(password))
+            //{
+            //    MessageBox.Show("Пожалуйста, заполните все поля.", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Warning);
+            //    return;
+            //}
+
+            //Users user = Database.AuthenticateUser(login, password);
+
+            //if (user != null)
+            //{
+            //    MessageBox.Show($"Добро пожаловать, {user.login}!", "Успех", MessageBoxButton.OK, MessageBoxImage.Information);
+            MainWindow.init.OpenPages(MainWindow.pages.main);
+            //}
+            //else
+            //{
+            //    MessageBox.Show("Неверный логин или пароль.", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+            //}
+        }
+    }
+}
