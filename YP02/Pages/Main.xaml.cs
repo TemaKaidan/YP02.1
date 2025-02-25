fe2257fb26bb2f8094317e53b9744e01e28bc128
Artem <artemmohov05@mail.ru>
2025-02-25 18:38:38 +0500

    Разработана страница Main

--- YP02/Pages/Main.xaml.cs
+++ YP02/Pages/Main.xaml.cs
@@ -9,6 +9,7 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
@@ -20,9 +21,110 @@ namespace YP02.Pages
     /// </summary>
     public partial class Main : Page
     {
+        private bool isMenuCollapsed = false;
         public Main()
         {
             InitializeComponent();
+        }
+
+        private void ToggleMenu(object sender, RoutedEventArgs e)
+        {
+            DoubleAnimation widthAnimation = new DoubleAnimation();
+
+            if (isMenuCollapsed)
+            {
+                widthAnimation.From = 50;
+                widthAnimation.To = 200;
+                MenuPanel.Width = 200;
+                foreach (UIElement element in MenuPanel.Children)
+                {
+                    if (element is Button btn && btn.Content.ToString() != "☰")
+                    {
+                        btn.Visibility = Visibility.Visible;
+                    }
+                }
+            }
+            else
+            {
+                widthAnimation.From = 200;
+                widthAnimation.To = 50;
+                foreach (UIElement element in MenuPanel.Children)
+                {
+                    if (element is Button btn && btn.Content.ToString() != "☰")
+                    {
+                        btn.Visibility = Visibility.Collapsed;
+                    }
+                }
+            }
+
+            widthAnimation.Duration = new Duration(TimeSpan.FromSeconds(0.3));
+            MenuPanel.BeginAnimation(WidthProperty, widthAnimation);
+            isMenuCollapsed = !isMenuCollapsed;
+        }
+
+        private void Click_Student(object sender, RoutedEventArgs e)
+        {
+            MainWindow.init.OpenPages(MainWindow.pages.student);
+        }
+
+        private void Click_Groups(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Click_Disciplines(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Click_DisciplinePrograms(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Click_TeachersLoad(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Click_Consultations(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Click_Absences(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Click_Teachers(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Click_Marks(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Click_ConsultationResults(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Click_LessonTypes(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Click_Roles(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Click_Users(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
